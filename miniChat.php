0c12c846df0a252749cc00f1da07c8f4760f8024
ValentinFontaine <valentin.fontaine@protonmail.com>
2018-12-19 06:36:09 +0100

    Ajout et affichage des messages fonctionnels

---
+++ miniChat.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Mon mini Chat</title>
+    </head>
+
+    <body>
+        <form action="miniChat_post.php" method="POST">
+            <p><label>Pseudo : </label><input type="input" name="pseudo" value="" /></p>
+            <p><label>Message : </label><input type="input" name="message" value="" /></p>
+            <p><input type="submit" value="Poster le message" /></p>
+        </form>
+
+        <?php 
+            try
+            { 
+                $bdd = new PDO('mysql:host=localhost;dbname=miniChat;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+            }
+            catch(Exception $e)
+            {
+                die('Erreur : ' . $e->getMessage());
+            }
+
+            //Affichage de tous les messages envoyés par la base
+            $requete = $bdd->query('SELECT pseudo, message FROM messages');
+            while ($message = $requete->fetch())
+            {
+                if(isset($message['pseudo']) AND isset($message['message']))
+                {
+                    echo '<p><strong>' . htmlspecialchars($message['pseudo']) . '</strong> : ' . htmlspecialchars($message['message']) . '</p>';
+                } 
+
+            }
+        ?>
+
+
+    </body>
+</html>
---
+++ miniChat_post.php
@@ -0,0 +1,27 @@
+<?php
+//Connexion à la base de donnée
+try
+{ 
+    $bdd = new PDO('mysql:host=localhost;dbname=miniChat;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+}
+catch(Exception $e)
+{
+    die('Erreur : ' . $e->getMessage());
+}
+
+
+//envoi du message à la base de données (si il y en a un) 
+if (isset($_POST['pseudo']) AND isset($_POST['message']) ) 
+{
+    $pseudo = htmlspecialchars($_POST['pseudo']);
+    $message = htmlspecialchars($_POST['message']);
+
+    if ($pseudo != '' AND $message != '')
+    {
+        $requete = $bdd->prepare('INSERT INTO messages(pseudo, message, date_message) VALUES(:pseudo, :message, NOW())');
+        $requete->execute(array('pseudo' => $pseudo, 'message' => $message));
+    }
+}
+
+header('Location: miniChat.php');
+?>
